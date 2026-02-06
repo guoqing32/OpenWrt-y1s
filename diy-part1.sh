a8b0f05cf425a8eec79dba37046c39c86987b200
guoqing32 <75204495+guoqing32@users.noreply.github.com>
2026-02-06 11:48:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
